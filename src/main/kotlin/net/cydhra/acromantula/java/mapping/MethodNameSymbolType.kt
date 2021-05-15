c4777e0cdebc5bb0ab396e8fae87461b6b8d174f
Cydhra <ubezl@student.kit.edu>
2021-05-15 18:10:51 +0200

    updated method signature to new acromantula version

--- src/main/kotlin/net/cydhra/acromantula/java/mapping/MethodNameSymbolType.kt
+++ src/main/kotlin/net/cydhra/acromantula/java/mapping/MethodNameSymbolType.kt
@@ -1,10 +1,10 @@
 package net.cydhra.acromantula.java.mapping
 
 import net.cydhra.acromantula.features.mapper.AcromantulaSymbolType
+import net.cydhra.acromantula.workspace.database.mapping.ContentMappingSymbol
 
 class MethodNameSymbolType : AcromantulaSymbolType("java.method.name", true) {
-    override fun onUpdateName(newName: String) {
+    override fun onUpdateName(symbol: ContentMappingSymbol, newName: String) {
         TODO("not implemented")
     }
-
 }
