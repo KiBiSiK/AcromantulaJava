b93e878f329e5a86dc87539f27034d8cfc5fce0e
Cydhra <ubezl@student.kit.edu>
2022-05-06 01:52:54 +0200

    implemented method renaming

--- src/main/kotlin/net/cydhra/acromantula/java/mapping/types/MethodInstructionReference.kt
+++ src/main/kotlin/net/cydhra/acromantula/java/mapping/types/MethodInstructionReference.kt
@@ -1,15 +1,16 @@
 package net.cydhra.acromantula.java.mapping.types
 
 import net.cydhra.acromantula.features.mapper.AcromantulaReferenceType
+import net.cydhra.acromantula.java.mapping.remapping.AsmRemappingHelper
 import net.cydhra.acromantula.workspace.database.mapping.ContentMappingReference
 import net.cydhra.acromantula.workspace.database.mapping.ContentMappingSymbol
 
 object MethodInstructionReference : AcromantulaReferenceType("java.insn.method") {
     override fun onUpdateSymbolName(symbol: ContentMappingSymbol, reference: ContentMappingReference, newName: String) {
-        TODO("not implemented")
+        AsmRemappingHelper.scheduleFileForRemapping(reference)
     }
 
     override fun stringRepresentation(ref: ContentMappingReference): String {
-        return ref.file.name + ": " + (ref.owner?.let { "[${it.name}]." } ?: "") + (ref.location ?: "")
+        return ref.file.name + ": INVOKE " + ref.symbol.identifier
     }
 }
--- src/main/kotlin/net/cydhra/acromantula/java/mapping/types/MethodNameSymbol.kt
+++ src/main/kotlin/net/cydhra/acromantula/java/mapping/types/MethodNameSymbol.kt
@@ -1,10 +1,55 @@
 package net.cydhra.acromantula.java.mapping.types
 
 import net.cydhra.acromantula.features.mapper.AcromantulaSymbolType
+import net.cydhra.acromantula.features.mapper.MapperFeature
+import net.cydhra.acromantula.java.mapping.remapping.AsmRemappingHelper
+import net.cydhra.acromantula.java.util.constructMethodIdentity
+import net.cydhra.acromantula.java.util.reconstructClassName
+import net.cydhra.acromantula.java.util.reconstructMethodDefinition
+import net.cydhra.acromantula.workspace.database.DatabaseMappingsManager
 import net.cydhra.acromantula.workspace.database.mapping.ContentMappingSymbol
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.objectweb.asm.commons.Remapper
 
 object MethodNameSymbol : AcromantulaSymbolType("java.method.name", true) {
     override fun onUpdateName(symbol: ContentMappingSymbol, newName: String) {
-        TODO("not implemented")
+        val (classIdentity, methodName, methodDescriptor) = reconstructMethodDefinition(symbol.identifier.value)
+
+        transaction {
+            val allReferences = MapperFeature.getReferences(symbol)
+            for (reference in allReferences) {
+                MapperFeature.getReferenceType(reference.type).onUpdateSymbolName(symbol, reference, newName)
+            }
+
+            AsmRemappingHelper.remapScheduledFiles(
+                symbol,
+                MethodNameRemapper(reconstructClassName(classIdentity), methodName, methodDescriptor, newName)
+            )
+
+            DatabaseMappingsManager.updateSymbolName(symbol, newName)
+            DatabaseMappingsManager.updateSymbolIdentifier(
+                symbol,
+                constructMethodIdentity(classIdentity, newName, methodDescriptor)
+            )
+        }
+    }
+
+    /**
+     * Remaps all references to a method to a new name
+     */
+    class MethodNameRemapper(
+        private val owner: String,
+        private val oldName: String,
+        private val descriptor: String,
+        private val newName: String
+    ) : Remapper() {
+
+        override fun mapMethodName(owner: String, name: String, descriptor: String): String {
+            if (owner == this.owner && name == this.oldName && descriptor == this.descriptor) {
+                return newName
+            }
+
+            return super.mapMethodName(owner, name, descriptor)
+        }
     }
 }
