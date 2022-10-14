12f934a98472f00d85c498350bc0931c8a5f76dd
Cydhra <ubezl@student.kit.edu>
2022-10-14 17:39:27 +0200

    implemented MethodNameSymbol

--- src/main/kotlin/net/cydhra/acromantula/java/mapping/types/MethodNameSymbol.kt
+++ src/main/kotlin/net/cydhra/acromantula/java/mapping/types/MethodNameSymbol.kt
@@ -1,37 +1,73 @@
 package net.cydhra.acromantula.java.mapping.types
 
-import net.cydhra.acromantula.features.mapper.AcromantulaSymbolType
+import net.cydhra.acromantula.features.mapper.AcromantulaSymbol
 import net.cydhra.acromantula.features.mapper.MapperFeature
+import net.cydhra.acromantula.java.mapping.database.JavaIdentifier
+import net.cydhra.acromantula.java.mapping.database.JavaIdentifierTable
 import net.cydhra.acromantula.java.mapping.remapping.AsmRemappingHelper
 import net.cydhra.acromantula.java.util.constructMethodIdentity
 import net.cydhra.acromantula.java.util.reconstructClassName
 import net.cydhra.acromantula.java.util.reconstructMethodDefinition
-import net.cydhra.acromantula.workspace.database.DatabaseMappingsManager
-import net.cydhra.acromantula.workspace.database.mapping.ContentMappingSymbol
+import net.cydhra.acromantula.workspace.filesystem.FileEntity
+import net.cydhra.acromantula.workspace.filesystem.FileTable
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.objectweb.asm.commons.Remapper
 
-object MethodNameSymbol : AcromantulaSymbolType("java.method.name", true) {
-    override fun onUpdateName(symbol: ContentMappingSymbol, newName: String) {
-        val (classIdentity, methodName, methodDescriptor) = reconstructMethodDefinition(symbol.identifier.value)
+object JavaMethodTable : IntIdTable() {
+    val identifier = reference("identifier", JavaIdentifierTable)
+    val name = varchar("name", Short.MAX_VALUE.toInt())
+    val sourceFile = reference("file", FileTable)
+}
+
+class MethodNameSymbol(id: EntityID<Int>) : IntEntity(id), AcromantulaSymbol {
+    companion object : IntEntityClass<MethodNameSymbol>(JavaMethodTable)
+
+    override val canBeRenamed: Boolean
+        get() = true
+
+    /**
+     * Unique java symbol identifier entity. Access with database transaction
+     */
+    var identifier by JavaIdentifier referencedOn JavaMethodTable.identifier
+
+    override var sourceFile by FileEntity referencedOn JavaMethodTable.sourceFile
+
+    /**
+     * Method name. Update only via [updateName]
+     */
+    var methodName by JavaMethodTable.name
+
+    override fun getName(): String {
+        return methodName
+    }
+
+    override suspend fun updateName(newName: String) {
+        val (classIdentity, _, methodDescriptor) = transaction {
+            reconstructMethodDefinition(identifier.value)
+        }
+
+        val allReferences = MapperFeature.getReferencesToSymbol(this)
+        for (reference in allReferences) {
+            reference.onUpdateSymbolName(newName)
+        }
+
+        AsmRemappingHelper.remapScheduledFiles(
+            this,
+            MethodNameRemapper(reconstructClassName(classIdentity), methodName, methodDescriptor, newName)
+        )
 
         transaction {
-            val allReferences = MapperFeature.getReferences(symbol)
-            for (reference in allReferences) {
-                MapperFeature.getReferenceType(reference.type).onUpdateSymbolName(symbol, reference, newName)
-            }
-
-            AsmRemappingHelper.remapScheduledFiles(
-                symbol,
-                MethodNameRemapper(reconstructClassName(classIdentity), methodName, methodDescriptor, newName)
-            )
-
-            DatabaseMappingsManager.updateSymbolName(symbol, newName)
-            DatabaseMappingsManager.updateSymbolIdentifier(
-                symbol,
-                constructMethodIdentity(classIdentity, newName, methodDescriptor)
-            )
+            methodName = newName
+            identifier.value = constructMethodIdentity(classIdentity, newName, methodDescriptor)
         }
+    }
+
+    override fun displayString(): String {
+        return "visibility returnType $methodName descriptor"
     }
 
     /**
